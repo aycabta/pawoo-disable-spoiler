dd1e8d9117523eba86c18826af77f3244087e49c
Code Ass <aycabta@gmail.com>
2017-04-21 18:45:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Pawoo Disable Spoiler
+=====================
+
+[This Greasemonkey script on OpenUserJS.org](https://openuserjs.org/scripts/aycabta/Pawoo_Disable_Spoiler)
---
+++ pawoo-disable-spoiler.user.js
@@ -0,0 +1,21 @@
+﻿// ==UserScript==
+// @name       Pawoo Disable Spoiler
+// @namespace  http://aycabta.github.io/
+// @version    0.0.1
+// @description  Pawooooooooooooooo
+// @include    /^https:\/\/pawoo.net/
+// @copyright  2017+, You
+// ==/UserScript==
+
+(function() {
+    var sheet;
+    var style = document.createElement('style');
+    style.setAttribute('id', 'pawoo-disable-spoiler-css');
+    style.type = "text/css";
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(style);
+    sheet = style.sheet;
+    sheet.insertRule('.media-spoiler {' +
+                     '    display: none !important;' +
+                     '}', sheet.cssRules.length);
+})();
