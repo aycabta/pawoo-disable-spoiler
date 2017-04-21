9825541bea554d7f452631c2ec6e1ce3a761745f
Code Ass <aycabta@gmail.com>
2017-04-21 23:29:34 +0900

    Support TL

--- pawoo-disable-spoiler.user.js
+++ pawoo-disable-spoiler.user.js
@@ -18,4 +18,27 @@
     sheet.insertRule('.media-spoiler {' +
                      '    display: none !important;' +
                      '}', sheet.cssRules.length);
+    var disableSpoiler = () => {
+        setTimeout(() => {
+            var spoilers = document.querySelectorAll('.media-spoiler');
+            spoilers.forEach(spoiler => {
+                var container = spoiler.parentElement;
+                var ev = document.createEvent('MouseEvents');
+                ev.initEvent('click', true, true);
+                spoiler.dispatchEvent(ev);
+                var button = container.querySelector('button');
+                button.parentElement.removeChild(button);
+            });
+        }, 200);
+    };
+    var observer = new MutationObserver(mutations => {
+        mutations.forEach(m => {
+            if (m.addedNodes !== undefined) {
+                m.addedNodes.forEach(n => {
+                    disableSpoiler(n);
+                });
+            }
+        });
+    });
+    observer.observe(document.querySelector('body'), { childList: true, subtree: true });
 })();
